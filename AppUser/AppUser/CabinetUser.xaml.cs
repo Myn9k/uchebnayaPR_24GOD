a34d6c92a414163784c6abf38ebf630e114ecc88
Myn9k <rodionpolyakov123@gmail.com>
2024-05-12 18:26:55 +0900

    Половина Планирования событий сделанно

--- AppUser/AppUser/CabinetUser.xaml.cs
+++ AppUser/AppUser/CabinetUser.xaml.cs
@@ -1,29 +1,22 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace AppUser
 {
     public partial class CabinetUser : Window
     {
         ApplicationContext db;
-        AuthWindows authWin = new AuthWindows();
 
         public CabinetUser()
         {
             InitializeComponent();
-
+            LoadEvents();
         }
+
         public void GetIdUser(int Id)
         {
             db = new ApplicationContext();
@@ -38,6 +31,45 @@ namespace AppUser
                 }
             }
             LoginUser.Text = strUser;
+        }
+        public class Event
+        {
+            public DateTime Date { get; set; }
+            public string Description { get; set; }
+        }
+        public void SaveEventsToJson(List<Event> events)
+        {
+            string json = JsonConvert.SerializeObject(events);
+            File.WriteAllText("events.json", json);
+        }
+        private void LoadEvents()
+        {
+            if (File.Exists("events.json"))
+            {
+                string json = File.ReadAllText("events.json");
+                List<Event> events = JsonConvert.DeserializeObject<List<Event>>(json);
+                EventsListBox.ItemsSource = events;
+            }
+        }
+        private void SaveEventButton_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime date = EventDatePicker.SelectedDate ?? DateTime.Now;
+            string description = EventDescriptionTextBox.Text;
+            Event newEvent = new Event
+            {
+                Date = date,
+                Description = description
+            };
+            List<Event> events = new List<Event>();
+            if (File.Exists("events.json"))
+            {
+                string json = File.ReadAllText("events.json");
+                events = JsonConvert.DeserializeObject<List<Event>>(json);
+            }
+            events.Add(newEvent);
+            SaveEventsToJson(events);
+            LoadEvents();
+            MessageBox.Show("Событие сохранено.");
         }
     }
 }
